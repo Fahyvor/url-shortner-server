b1f0e93eedccc3d68181b870e0a705b57aa6e0a5
Fahyvor <favourokafor30@gmail.com>
2026-03-15 18:25:14 +0100

    javascript runtime for  youtube

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -63,6 +63,8 @@ exports.downloadVideo = async (req, res) => {
     ytDlp
       .execStream([
         url,
+        "--js-runtimes",
+        "node",
         "--no-playlist",
         "--user-agent",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36",
